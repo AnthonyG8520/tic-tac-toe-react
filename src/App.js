a8fce76315424ea56d85f2ab04283e172d749fc8
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2024-05-03 13:58:39 -0500

    can now place x and o without replacing the current letter in the square

--- src/App.js
+++ src/App.js
@@ -2,32 +2,49 @@ import {useState} from "react";
 
 export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null))
+    const [xIsNext, setXIsNext] = useState(true)
 
     function handleClick(squareToUpdate){
+
+        // this conditional prevents another letter being placed
+        // in the square if one is already present
+        if(squares[squareToUpdate]){
+            return;
+        }
         const nextSquares = squares.slice();
-        nextSquares[squareToUpdate] = 'x';
-        setSquares(nextSquares)
+
+        //this conditional tell the board when to put an X or an O
+        // based off of the state of the XIsNext boolean
+        // after X or O is used the state of the variable is flipped
+        if(xIsNext){
+            nextSquares[squareToUpdate] = 'X'
+        }else{
+            nextSquares[squareToUpdate] = 'O'
+        }
+
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
 
-  return(
-      <>
-          <div className="board-row">
-              <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-              <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-              <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-          </div>
-          <div className="board-row">
-              <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-              <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-              <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-          </div>
-          <div className="board-row">
-              <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-              <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-              <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-          </div>
-      </>
-  );
+      return(
+          <>
+              <div className="board-row">
+                  <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+                  <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+                  <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+              </div>
+              <div className="board-row">
+                  <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+                  <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+                  <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+              </div>
+              <div className="board-row">
+                  <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+                  <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+                  <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+              </div>
+          </>
+      );
 }
 
 
