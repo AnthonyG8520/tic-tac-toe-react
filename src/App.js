1acda526bd28f87afc09cc6bbed06d77e69f3bdf
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2024-06-04 10:58:26 -0500

    added more code for lookig back at moves

--- src/App.js
+++ src/App.js
@@ -13,6 +13,23 @@ export default function Game(){
         setXIsNext(!xIsNext);
     }
 
+    function jumpTo(nextMove){
+
+    }
+
+    const moves = history.map((squares, move) =>{
+        let description;
+        if(move > 0){
+            description = 'Go to move #' + move;
+        }else{
+            description = 'Go to game start';
+        }
+        return(
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+    });
 
     return (
         <div className="game">
@@ -20,7 +37,7 @@ export default function Game(){
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
             </div>
             <div className="game-info">
-                <ol>{/*TODO*/}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
