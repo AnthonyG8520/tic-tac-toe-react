75c505cccaf336fc7e1da6b5a189206bcca0749e
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2024-06-07 10:10:07 -0500

    restarting at lifting state up again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import {useState} from "react";
 
-export default function Board() {
+
+export default function Game() {
+
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSqaures = history[history.length - 1];
+
+    function handlePlay(nextSquares){
+
+    }
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSqaures} onPlay={handlePlay}/>
+            </div>
+            <div className="game-info">
+                <ol>{/*TODO*/}</ol>
+            </div>
+        </div>
+    );
+}
+
+function Board({xIsNext, squares, onPlay}) {
     const [squares, setSquares] = useState(Array(9).fill(null))
     const [xIsNext, setXIsNext] = useState(true)
 
